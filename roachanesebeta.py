3b8ac038b475eab69eda164e33e84c0ddebe2f8b
tanmay z <50272014+accidentallogger@users.noreply.github.com>
2021-11-22 22:40:03 +0530

    Add files via upload

---
+++ roachanesebeta.py
@@ -0,0 +1,20 @@
+import time
+
+
+print(" THIS IS ROACHANESE BITCH ")
+while (0<1):
+    print(" ")
+    x=input("\n hello pls tell me what should i convert to roachanese : " )
+    z=x.split(" ") 
+    qp=len(z)
+    b=str("ROACH")
+    time.sleep(1)
+    
+    print('\n converting \n')
+            
+    time.sleep(1)
+    for q in range(0,qp):
+        if (z[q].upper())==("BROTHER"):
+            print(z[q],end=" ")
+        else:
+            print(z[q]+b,end=" ")
